78c54da1487ea1d475b84904ecdd66282f7e9b4a
Andy Kleindienst <andy.kleindienst@gmail.com>
2022-12-29 13:49:23 -0500

    added hour divs in html, created function to save data from each hour element

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +23,24 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+
+$('.saveBtn').click(function () {
+  console.log("the save button function is being heard")
+  let hourNumber = $(this).parent().attr('id')
+  console.log(hourNumber)
+
+
+
+  let hourText = $(this).siblings('.description').val();
+  console.log(hourText);
+  alert(hourText);
+
+  localStorage.setItem(hourNumber, hourText)
+
+
+
+
+  // localStorage.setItem("taskAtEleven", hourText)
+})
